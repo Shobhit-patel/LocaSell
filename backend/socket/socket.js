fa8796b230e7d98ac689cdd9dfe847b91798d61c
Shobhit Patel <patelshobhit194@gmail.com>
2026-07-17 08:14:02 +0530

    Update cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const onlineUsers = new Map();
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://loca-sell.vercel.app/",
             credentials: true,
         },
     });
